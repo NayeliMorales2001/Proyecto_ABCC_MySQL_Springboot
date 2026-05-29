555cf58b4d32286c63ae4f53483396489c2cf412
NayeliMorales19 <moralesnayeli120217@gmail.com>
2026-05-28 22:17:04 -0600

    agregue codigo al modelo de la materia

--- src/main/java/com/example/Proyecto_ABCC_MySQL/models/MateriaModel.java
+++ src/main/java/com/example/Proyecto_ABCC_MySQL/models/MateriaModel.java
@@ -1,5 +1,51 @@
 package com.example.Proyecto_ABCC_MySQL.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "materia_model")
 public class MateriaModel {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+    private String maestro;
+    private int creditos;
+
+    public MateriaModel() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getMaestro() {
+        return maestro;
+    }
+
+    public void setMaestro(String maestro) {
+        this.maestro = maestro;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
 }
