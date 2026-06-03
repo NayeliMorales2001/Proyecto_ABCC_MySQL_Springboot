a1cf632c51b80707a50efa682392f6bff0eb1d8e
NayeliMorales19 <moralesnayeli120217@gmail.com>
2026-06-02 20:51:22 -0600

    ccambios para que se muestre login

--- src/main/java/com/example/Proyecto_ABCC_MySQL/config/SecurityConfig.java
+++ src/main/java/com/example/Proyecto_ABCC_MySQL/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.Proyecto_ABCC_MySQL.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -14,9 +15,10 @@ public class SecurityConfig {
                 http
                                 .csrf(csrf -> csrf.disable())
                                 .authorizeHttpRequests(auth -> auth
-                                                .anyRequest().permitAll());
+                                                .requestMatchers("/health").permitAll()
+                                                .anyRequest().authenticated())
+                                .formLogin(Customizer.withDefaults());
 
                 return http.build();
         }
-
 }
